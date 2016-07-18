5480902fb484192a943bd0560b5df16cbf807b55
fxue <xuefengwork@qq.com>
2016-07-18 10:37:00 +0800

    no mkdir

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "hello fxue, I am in a bash shell file."
-echo "TEST_FOLDER IS: ${TEST_FOLDER}"
-mkdir -p ${TEST_FOLDER}
+#echo "TEST_FOLDER IS: ${TEST_FOLDER}"
+#mkdir -p ${TEST_FOLDER}
